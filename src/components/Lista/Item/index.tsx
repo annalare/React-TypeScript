f5f0d9f2982f1aa8f99863693c54e053a5ce818b
Larissa Mendes <83883968+annalare@users.noreply.github.com>
2022-08-26 14:03:01 -0300

    feat: itens da lista

--- src/components/Lista/Item/index.tsx
+++ src/components/Lista/Item/index.tsx
@@ -1,14 +1,31 @@
 import { Itarefa } from "../../../types/tarefa";
 import style from "../Lista.module.scss";
+
+interface Props extends Itarefa {
+  selecionaTarefa: (tarefaSelecionada: Itarefa) => void;
+}
+
 export default function Item({
   tarefa,
   tempo,
   selecionado,
   completado,
   id,
-}: Itarefa) {
+  selecionaTarefa,
+}: Props) {
   return (
-    <li className={style.item}>
+    <li
+      className={`${style.item} ${selecionado ? style.itemSelecionado : ""}`}
+      onClick={() =>
+        selecionaTarefa({
+          tarefa,
+          tempo,
+          selecionado,
+          completado,
+          id,
+        })
+      }
+    >
       <h3>{tarefa}</h3>
       <span>{tempo}</span>
     </li>
--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -2,13 +2,17 @@ import { Itarefa } from "../../types/tarefa";
 import Item from "./Item";
 import style from "./Lista.module.scss";
 
-function Lista({ tarefas }: { tarefas: Itarefa[] }) {
+interface Props {
+  tarefas: Itarefa[];
+  selecionaTarefa: (tarefaSelecionada: Itarefa) => void;
+}
+function Lista({ tarefas, selecionaTarefa }: Props) {
   return (
     <aside className={style.listaTarefas}>
       <h2>Estudos do dia</h2>
       <ul>
-        {tarefas.map((item, index) => (
-          <Item key={index} {...item} />
+        {tarefas.map((item) => (
+          <Item selecionaTarefa={selecionaTarefa} key={item.id} {...item} />
         ))}
       </ul>
     </aside>
